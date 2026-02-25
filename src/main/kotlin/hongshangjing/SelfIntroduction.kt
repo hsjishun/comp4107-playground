f10cca8580b612e871617d3be63844c4fdcc1166
HONG Shangjing <23259124@life.hkbu.edu.hk>
2026-02-25 17:35:35 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/hongshangjing/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.hongshangjing
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is HONG Shangjing.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
